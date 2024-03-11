999b00932362183b6fbb27e33f2637f1543708af
Craig Stowers <craig.stowers@outlook.com>
2024-03-11 17:43:27 +1100

    removed logos

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
    return (
       <>
-         <div>
-            <a href="https://vitejs.dev" target="_blank">
-               <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-               <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-         </div>
+         <div>NO LOGOS</div>
          <h1>Vite + React DEV</h1>
          <div className="card">
             <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
