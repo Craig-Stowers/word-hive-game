36159ac3ece1d5131766b932246695af156fa1e9
Craig Stowers <craig.stowers@outlook.com>
2024-04-23 13:12:28 +1200

    .

--- src/components/layouts/WithHeader.jsx
+++ src/components/layouts/WithHeader.jsx
@@ -1,7 +1,7 @@
 import Header from "../shared/Header";
 const WithHeader = ({ children, ...props }) => {
    return (
-      <div style={{ display: "flex", flexDirection: "column", height: "100vh", minHeight: "533px" }}>
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
          <Header {...props} />
          <div style={{ flexGrow: 1, overflow: "hidden" }}>{children}</div>
       </div>
