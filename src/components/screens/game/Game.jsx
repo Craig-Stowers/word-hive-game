a76ab9060cf4dae07aaa005423ca3bc27efe5a26
Craig Stowers <craig.stowers@gmail.com>
2024-06-25 14:46:46 +1000

    .

--- src/components/screens/game/Game.jsx
+++ src/components/screens/game/Game.jsx
@@ -147,13 +147,12 @@ const Game = forwardRef(({ screen, dataIndex = 3, size }, ref) => {
         setLetters(filteredLetters);
         setAllLettersArr([key.toUpperCase(), ...filteredLetters.split("")]);
 
-        console.log("reshuffle everything", storedData.shuffledLetters);
-
         const availableAnswers = data.answers.map((word) => word.toLowerCase()); //possibly remove as capitals could be good indicator of plural (unless game data filtered first)
         setAvailableAnswers(availableAnswers);
         const shuffled =
             storedData &&
             storedData.shuffledLetters &&
+            Array.isArray(storedData.shuffledLetters) &&
             storedData.shuffledLetters.length === 6
                 ? storedData.shuffledLetters
                 : shuffleArray(filteredLetters.split(""));
